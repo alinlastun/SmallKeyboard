75b622ed222c5a45542d1ce3f9718ecd5adff4dc
Alin Lastun <alinlastun@gmail.com>
2019-03-02 14:12:18 +0200

    first commit

---
+++ build/generated/source/buildConfig/keyboard_simple_blue/release/com/themejunky/small/keyboard/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.themejunky.small.keyboard;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.jb.gokeyboard.theme.BestThemes.simpleblue";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "keyboard_simple_blue";
+  public static final int VERSION_CODE = 59;
+  public static final String VERSION_NAME = "3.0.59";
+}
